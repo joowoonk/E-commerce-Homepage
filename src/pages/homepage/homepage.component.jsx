9c828fd4958c2fb6aa1cd6d9f0d19afc162607fb
Golden Eagle <joowoon.kang@biola.edu>
2020-03-22 20:35:10 -0700

    added backgound image onto menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import "./homepage.styles.scss"
+
+import Directory from "../../components/directory/directory.component"
+
+
+
+
+// import MenuItem from "./pages/homepage/homepage.component"
+
+const HomePage = () => {
+    return (
+        <div className = 'homepage'> 
+       
+           <Directory />
+           
+        </div>
+  
+    )
+}
+
+export default HomePage
