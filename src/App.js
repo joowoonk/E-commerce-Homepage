01a4aedf863e421e0c364e5621d95c1351f22728
Golden Eagle <joowoon.kang@biola.edu>
2020-03-23 20:41:22 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
+
+import { Switch, Route, Link} from 'react-router-dom'
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <Link to='/'> Homapge</Link>
+      <button onClick ={()=> props.history.push('/')}>Take me homepage</button>
+      <h1>HATS PAGE</h1>
+    </div>
+
+  )
+}
+
+  
+
 
 class App extends Component {
   render() {
     return(
       <div className='App'>
-       <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' component={HatsPage} />
+        </Switch>
       </div>
     )
   
