edab60d199ea4278cf05f113bda7be5b640e35b6
Golden Eagle <joowoon.kang@biola.edu>
2020-03-24 23:28:45 -0700

    added firebase to sign in button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import { Switch, Route} from 'react-router-dom'
 import './App.css';
@@ -7,11 +7,35 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 
 import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component'
+import { auth } from './firebase/firebase.utils'
 
   
 
 
-class App extends Component {
+class App extends React.Component {
+ constructor(){
+   super();
+
+   this.state = {
+     currentUser: null
+   };
+ }
+
+ unsubscribeFromAuth = null;
+
+ componentDidMount() {
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+     this.setState({ currentUser: user });
+
+     console.log(user);
+   });
+ }
+
+ componentWillUnmount() {
+   this.unsubscribeFromAuth();
+ }
+
+
   render() {
     return(
       <div className='App'>
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -6,6 +6,8 @@ import FormInput from '../form-input/form-input.component'
 
 import CustomButton from '../custom-button/custom-button.component'
 
+import { signInWithGoogle } from '../../firebase/firebase.utils'
+
 
 class SignIn extends React.Component{
     constructor(props){
@@ -56,6 +58,10 @@ class SignIn extends React.Component{
              
 
                     <CustomButton type="submit">Sign In</CustomButton>
+                    <CustomButton onClick={signInWithGoogle} > 
+                    {' '}
+                    Sign in with Google{' '}
+                    </CustomButton>
                 </form>
             </div>
         )
