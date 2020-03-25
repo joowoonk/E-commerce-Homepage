b0934d36ef325218736c1ef92f038408be1dc377
Golden Eagle <joowoon.kang@biola.edu>
2020-03-24 22:11:37 -0700

    added sign in button with styling.

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -4,6 +4,8 @@ import './sign-in.styles.scss'
 
 import FormInput from '../form-input/form-input.component'
 
+import CustomButton from '../custom-button/custom-button.component'
+
 
 class SignIn extends React.Component{
     constructor(props){
@@ -53,7 +55,7 @@ class SignIn extends React.Component{
                     />
              
 
-                    <input type="submit" value="Submit Form" />
+                    <CustomButton type="submit">Sign In</CustomButton>
                 </form>
             </div>
         )
